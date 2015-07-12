e91ac5373fc05e7f11c535e12b2d8d249b3acbdf
vzhukovsky <757706zuk5404>
2015-07-12 14:28:32 +0300

    add express suport

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+var express = require('express');
+
 var app         = express();
+
+var port = 9000;
 
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
+
+app.listen(port);
